08b34c477f5808fca7cd463410a1ae926bacd805
mikaluuv <r1060214@student.thomasmore.be>
2025-02-10 11:00:59 +0100

    everything

--- topics/fundamentals.starter.js
+++ topics/fundamentals.starter.js
@@ -8,33 +8,72 @@
 // 1. link this script file to your page and open your developer console
 
 // 2. create a variable that keeps track of your name (can be let, const or var)
+let username = prompt("Name?", "Bob");
 
 // 3. create a variable that asks for your birthyear (use prompt() for that)
 // hint: prompt() returns a string, so you'll need to convert it to a number with parseInt()
+let birthyear = parseInt(prompt("Birthyear?", "2000"));
 
 // 4. calculate your age (this will require grabbing the current year somehow)
 // hint: you can get the current year with new Date().getFullYear()
-
+let age = new Date().getFullYear() - birthyear;
 // 5. log your age to the console
-
+console.log(age);
 // 6. write a function named greet() that wishes you a nice day like `Hi there, [your name]! Have a nice day!`
 // hint: you can use string interpolation with backticks to insert your name into the string
 // hint: the function should take [your name] as an argument
 // use the function to print the message to the console
-
+function greet(name) {
+  console.log(`Hello ${name}, have a nice day.`);
+}
+greet("Bob");
 // 7. write a function named calculateAge() that calculates your age and logs it to the console
 // use the function to print your age to the console
 // hint: the function should take your birthyear as an argument
-
+function calculateAge(birthyear) {
+  let currentyear = new Date().getFullYear();
+  let age = currentyear - parseInt(birthyear);
+  console.log(age);
+}
+calculateAge("2000");
 // 8. use prompt() to ask if you how you are feeling today ("good", "bad", "ok")
 // store the result in a variable
 // print an emoji to the console based on the result (😊, 😞, 😐)
 // kudos to you if you use a switch statement for this
+function feeling() {
+  let feeling = prompt("How are you feeling?", "Good");
+  switch (feeling.toLowerCase()) {
+    case "good":
+      console.log("It's good that you're feeling good.");
+      break;
+    case "bad":
+      console.log("Awh hope you feel better soon!");
+      break;
+    case "ok":
+      console.log("ok.");
+      break;
+    default:
+      console.log("I don't have a response for this. (Except this one)");
+      break;
+  }
+}
+feeling();
 
 // 9. create an array `foods` with three of your favorite foods
 // loop through the array and log each food to the console (use a for loop)
 // hint: you can get the length of an array with `array.length`
 
+let foods = ["rice and chicken", "Fries", "Boulette"];
+for (let i = 0; i < foods.length; i++) {
+  console.log(foods[i]);
+}
+
 // 10. create an object `person` with properties `name`, `age` and `favoriteFoods`
 // use the array `foods` as the value for the `favoriteFoods` property
 // log the object to the console
+const person = {
+  name: username,
+  age: age,
+  favoriteFoods: foods,
+};
+console.log(person);
